f71f87da15d5a6f6b280dafe5bbb76634968f05d
HANNAH <HUPEE2001@YAHOO.COM>
2024-04-15 16:04:07 +0100

    summary

---
+++ server/books.js
@@ -0,0 +1,46 @@
+const books = [
+  {
+    title: "The Autobiography of Malcolm X",
+    author: "by Malcolm X and Alex Haley ",
+    genreid: "11",
+    imglink: "https://productimages.worldofbooks.com/0345350685.jpg",
+    summary:
+      "The Autobiography of Malcolm X is a powerful and candid memoir that traces the life of Malcolm X, from his tumultuous childhood to his transformation into one of the most influential civil rights leaders in American history. Co-written by Alex Haley, the book offers a firsthand account of Malcolm X's journey from a troubled youth involved in crime to his conversion to Islam while serving time in prison. It delves into his experiences with racism, his time as a prominent figure in the Nation of Islam, and his eventual break with the organization. Through his reflections, Malcolm X shares his evolving beliefs on race, religion, and activism, offering valuable insights into the struggles faced by African Americans during the civil rights era. His autobiography is a compelling narrative of self-discovery, resilience, and the quest for justice.",
+  },
+
+  {
+    title: "Long Walk to Freedom",
+    author: "by Nelson Mandela",
+    genreid: "11",
+    imglink: "https://productimages.worldofbooks.com/0349106533.jpg",
+    summary:
+      "Long Walk to Freedom is Nelson Mandela's deeply moving autobiography that chronicles his extraordinary life journey. From his childhood in a rural village to his pivotal role in ending apartheid and becoming South Africa's first democratically elected president, Mandela's story is one of resilience, courage, and unwavering commitment to justice. Throughout the book, Mandela shares intimate details of his struggles, sacrifices, and triumphs, including his 27 years of imprisonment on Robben Island. With honesty and humility, he reflects on his personal growth, political activism, and the challenges of leading a nation torn apart by racial injustice.",
+  },
+
+  {
+    title: "My Life",
+    author: "by Bill Clinton",
+    genreid: "11",
+    imglink: "https://productimages.worldofbooks.com/0375414576.jpg",
+    summary:
+      "My Life is the memoir of Bill Clinton, the 42nd President of the United States. In this candid and comprehensive account, Clinton reflects on his childhood in Arkansas, his rise in politics, and his time in the White House. From his early days as a student leader to his tenure as Governor of Arkansas and ultimately to his presidency, Clinton provides insights into the pivotal moments and decisions that shaped his political career. He discusses his achievements, such as economic prosperity and welfare reform, as well as challenges and controversies, including impeachment proceedings. Through personal anecdotes and political analysis, Clinton offers readers a nuanced perspective on his presidency and the complex issues facing the nation during his time in office.",
+  },
+
+  {
+    title: "Bossypants",
+    author: "by Tina Fey",
+    genreid: "11",
+    imglink: "https://productimages.worldofbooks.com/0751547832.jpg",
+    summary:
+      "Bossypants is Tina Fey's witty and insightful memoir that takes readers on a humorous journey through her life and career in comedy. From her childhood in Pennsylvania to her rise to fame as a writer, actress, and producer on Saturday Night Live and 30 Rock, Fey shares candid anecdotes and reflections on navigating the male-dominated world of comedy. With her trademark humor and self-deprecating charm, she offers behind-the-scenes glimpses into her experiences in show business, including memorable encounters with colleagues and celebrities. Along the way, Fey imparts valuable lessons on confidence, ambition, and the importance of staying true to oneself.",
+  },
+
+  {
+    title: "I Know Why the Caged Bird Sings",
+    author: "by Maya Angelo",
+    genreid: "11",
+    imglink: "https://productimages.worldofbooks.com/1405882654.jpg",
+    summary:
+      "I Know Why the Caged Bird Sings is Maya Angelou's powerful and poignant memoir, recounting her childhood and adolescence in the racially segregated South. The book explores themes of racism, identity, trauma, and resilience as Angelou shares her experiences of growing up amidst poverty, abuse, and prejudice. Despite facing numerous challenges, including sexual assault and the struggle for acceptance, Angelou finds solace and strength in literature and poetry. Through her eloquent prose and vivid storytelling, she emerges as a symbol of triumph over adversity, inspiring readers with her courage, wisdom, and indomitable spirit.",
+  },
+];
